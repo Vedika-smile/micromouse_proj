96e0160a06ffcd80d73aeab3b6aac665b4c00b5f
Vedika-smile <vedikamalpani38@gmail.com>
2024-10-21 22:10:15 +0530

    Update floofill.h

--- floofill.h
+++ floofill.h
@@ -1,1 +1,25 @@
 
+byte orientation(byte orient, char turning){
+  if (turning== 'L'){
+        orient=orient-1;
+        if (orient==-1|| orient== 255)
+            orient=3;
+  }
+    else if(turning== 'R'){
+        orient=orient+1;
+        if (orient==4|| orient==255)
+            orient=0;
+    }
+    else if(turning== 'B'){
+        if (orient==0)
+            orient=2;
+        else if (orient==1)
+            orient=3;
+        else if (orient==2)
+            orient=0;
+        else if (orient==3)
+            orient=1;
+    }
+
+    return(orient);
+}
