98544a6486be0834d1ce015ac6f279f2e1c2d16b
Vedika-smile <vedikamalpani38@gmail.com>
2024-10-21 22:25:54 +0530

    Update mouseposition.h

--- mouseposition.h
+++ mouseposition.h
@@ -37,3 +37,80 @@ byte updateCoordinates(){//byte x, byte y, byte orient){
 
     //return(x,y);
 }
+void updateWalls(byte x, byte y, byte orient, boolean L, boolean R, boolean F){
+    if(L && R && F){
+        if (orient==0)
+            cells[y][x]= 13;
+        else if (orient==1)
+            cells[y][x]= 12;
+        else if (orient==2)
+            cells[y][x]= 11;
+        else if (orient==3)
+            cells[y][x]= 14;
+    }
+
+    else if (L && R && !F){
+        if (orient==0 || orient== 2)
+            cells[y][x]= 9;
+        else if (orient==1 || orient==3)
+            cells[y][x]= 10;
+    }
+    else if (L && F && !R){
+        if (orient==0)
+            cells[y][x]= 8;
+        else if (orient==1)
+            cells[y][x]= 7;
+        else if (orient==2)
+            cells[y][x]= 6;
+        else if (orient==3)
+            cells[y][x]= 5;
+    }
+
+    else if (R && F && !L){
+        if (orient==0)
+            cells[y][x]= 7;
+        else if (orient==1)
+            cells[y][x]= 6;
+        else if (orient==2)
+            cells[y][x]= 5;
+        else if (orient==3)
+            cells[y][x]= 8;
+    }
+    else if(F){
+        if (orient==0)
+            cells[y][x]= 2;
+        else if (orient==1)
+            cells[y][x]= 3;
+        else if (orient==2)
+            cells[y][x]= 4;
+        else if (orient==3)
+            cells[y][x]= 1;
+    }
+
+    else if(L){
+        if (orient==0)
+            cells[y][x]= 1;
+        else if (orient==1)
+            cells[y][x]= 2;
+        else if (orient==2)
+            cells[y][x]= 3;
+        else if (orient==3)
+            cells[y][x]= 4;
+    }
+
+    else if(R){
+        if (orient==0)
+            cells[y][x]= 3;
+        else if (orient==1)
+            cells[y][x]= 4;
+        else if (orient==2)
+            cells[y][x]= 1;
+        else if (orient==3)
+            cells[y][x]= 2;
+    }
+    else
+        cells[y][x]= 15;
+
+
+}
+
